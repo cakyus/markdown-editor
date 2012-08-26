bc66649d3afe69c41f69c9dfc2d2eefc4b0dfc9a
cakyus <cakyus@ph521.(none)>
2012-08-27 02:44:54 +0700

    enable multiple dot in js code, ie. "this.editor.showCode();"

--- mde.js
+++ mde.js
@@ -1,12 +1,19 @@
 
-var mde = function() {
-	this.editor = function() {
-		this.showCode = function() {}
+$(document).ready(function(){
+	(new mde).init();
+});
+
+function mde() {
+	this.init = function() {
+		this.editor.showCode();
+	}
+	this.editor = (new editor);
+	function editor() {
+		this.showCode = function() {
+			// @todo write something here ..
+		}
 		this.showHTML = function() {}
 		this.showPreview = function() {}
 	}
-	this.storage = function() {
-		this.save = function() {}
-		this.open = function() {}
-	}
 }
+
