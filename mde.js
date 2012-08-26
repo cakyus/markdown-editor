8f88b26432c473e25824b7406edc8f346f41c564
cakyus <cakyus@ph521.(none)>
2012-08-27 01:45:46 +0700

    add documentation

---
+++ mde.js
@@ -0,0 +1,12 @@
+
+var mde = function() {
+	this.editor = function() {
+		this.showCode = function() {}
+		this.showHTML = function() {}
+		this.showPreview = function() {}
+	}
+	this.storage = function() {
+		this.save = function() {}
+		this.open = function() {}
+	}
+}
